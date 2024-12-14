f106315a3b35fddbda84c1adcbe6108c7caf3406
Kabir Sharma <sharmakabir204@gmail.com>
2024-12-14 23:54:17 +0530

    implemented routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 
-import './App.css';
-
+import Body from './components/Body';
 function App() {
   return (
-    <div className="text-3xl font-bold text-red-700">
-      Namsate Everyone, let's build Netflix GPT.
-    </div>
+   <Body/>
   );
 }
 
